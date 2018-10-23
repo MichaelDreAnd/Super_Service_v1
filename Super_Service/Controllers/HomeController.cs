7016929218efbd34cc6d566e32379e8ca3100ffa
nick3925 <nick3925@student.eadania.dk>
2018-10-23 21:12:22 +0200

    Get haircut-companies from DB through Search view UI.

--- Super_Service/Controllers/HomeController.cs
+++ Super_Service/Controllers/HomeController.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Super_Service.Models;
 
 namespace Super_Service.Controllers
 {
@@ -15,6 +17,16 @@ namespace Super_Service.Controllers
 
         public IActionResult Search()
         {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Search(int zipCode, string city)
+        {
+            List<Company> result = DB.GetCompanies(zipCode, city);
+            ViewBag.lstCompanies = new MultiSelectList(result, "CompanyName", "Address", "ZipCode");
+            //ViewBag.Result = result;
+            //ViewBag.Result = $"{zipCode} + {city} = {result}";
             return View();
         }
 

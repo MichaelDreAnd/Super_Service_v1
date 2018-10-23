b0316dfcb1df1e28cbd3f146e27f438a43ffcf9e
Thomas Rønn Andersen <thom378w@student.eadania.dk>
2018-10-23 11:19:24 +0200

    CreateHairdresser

---
+++ Super_Service/Models/Hairdresser.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Super_Service.Models
+{
+    public class Hairdresser
+    {
+        public string Name { get; set; }
+        public int CompanyID { get; set; }
+        public string Address { get; set; }
+        public int ZipCode { get; set; }
+        public double Rating { get; set; }
+
+    }
+}

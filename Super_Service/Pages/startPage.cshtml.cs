d8b2ad1688ad7b292fc6f327570d8a2254b86a22
nick3925 <nick3925@student.eadania.dk>
2018-10-23 08:41:04 +0200

    Page added.

---
+++ Super_Service/Pages/startPage.cshtml.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Super_Service.Pages
+{
+    public class startPageModel : PageModel
+    {
+        public string Message { get; private set; } = "PageModel";
+        public void OnGet()
+        {
+            Message = "";
+        }
+    }
+}
